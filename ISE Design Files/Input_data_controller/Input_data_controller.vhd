53d556d546ade83707e296f6153ed6c76b938ab1
DEEPAK J PUTHUKKADEN <deepakjputhukkaden.mec@gmail.com>
2016-03-16 13:18:49 +0530

    after a while
    too looong before the last commit ;-)

---
+++ ISE Design Files/Input_data_controller/Input_data_controller.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Input_data_controller is
+	port(	data_in: in STD_LOGIC_VECTOR(3 downto 0);
+			data_ready : in STD_LOGIC);
+end Input_data_controller;
+
+architecture Input_data_controller_arch of Input_data_controller is
+
+begin
+
+
+end Input_data_controller_arch;
+
